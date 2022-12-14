d87f5c64153f9933ee30e9add527d5473353326e
cdavila <cristiand395@gmail.com>
2022-12-14 23:27:40 +0100

    adding tweet button

---
+++ script.js
@@ -0,0 +1,48 @@
+const quoteCOntainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const author = document.getElementById('author');
+const newQuoteButton = document.getElementById('new-quote');
+const twitterButton = document.getElementById('twitter');
+
+let quotes = []
+
+function getNewQuote() {
+    const quote = quotes[Math.floor(Math.random() * quotes.length)]
+    // Handle unknown author
+    if (!quote.author){
+        author.textContent = 'unknown author'
+    } else {
+        author.textContent = quote.author
+    }
+    // Handle long quote
+    if (quote.text.length > 100){
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text
+}
+
+//Get Quotes from API
+async function getQuotes(){
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
+    try{
+        const response = await fetch(apiUrl)
+        quotes = await response.json()
+        getNewQuote(quotes)
+    } catch(error) {
+        //Catch error here
+    }
+}
+
+//Tweet quote
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${author.textContent}`
+    window.open(twitterUrl, '_blank')
+}
+
+//Event Listeners
+newQuoteButton.addEventListener('click', getNewQuote)
+twitterButton.addEventListener('click', tweetQuote)
+
+getQuotes()
